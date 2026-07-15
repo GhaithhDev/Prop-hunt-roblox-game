53dbdbd6b975d742ad252940b26457033cae3276
GhaithhDev <GhaithDevPersonal@gmail.com>
2026-07-16 01:17:27 +0300

    release features

--- src/server/Data/init.lua
+++ src/server/Data/init.lua
@@ -9,7 +9,7 @@ local PressXpService = require(ServerScriptService.Server.PressXp.PressXpService
 local SpeedService = require(ServerScriptService.Server.Speed.SpeedService)
 
 local Players = game:GetService("Players")
-local Key = RunService:IsStudio() and "Studio_Store_15" or "Test_Store_3"
+local Key = RunService:IsStudio() and "Studio_Store_16" or "Test_Store_4"
 
 local PlayerStore = ProfileStore.New(Key, PROFILE_TEMPLATE)
 
