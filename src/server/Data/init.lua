367fbdfbeeddf07a83c2d7ceef9c13d34b45598e
GhaithhDev <GhaithDevPersonal@gmail.com>
2026-08-05 18:47:22 +0300

    Fixed leaderboards on world2 and 3 Fixed leveling up after 1999

--- src/server/Data/init.lua
+++ src/server/Data/init.lua
@@ -9,7 +9,7 @@ local PressXpService = require(ServerScriptService.Server.PressXp.PressXpService
 local InventoryService = require(ServerScriptService.Server.Inventory.InventoryService)
 
 local Players = game:GetService("Players")
-local Key = RunService:IsStudio() and "Studio_Store_40" or "Test_Store_40"
+local Key = RunService:IsStudio() and "Studio_Store_100" or "Test_Store_100"
 
 local PlayerStore = ProfileStore.New(Key, PROFILE_TEMPLATE)
 
